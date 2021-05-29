ee4e2e5e1a71badecca01a6927176f2c2020dc10
eduardobarros13 <eduardobarros.eng@outlook.com>
2021-05-29 19:30:37 -0300

    limpando cogigo free code camp

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,9 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'G-1HKTNMF3FH');
